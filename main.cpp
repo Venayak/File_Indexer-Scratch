aed857e4296486ee081f5a3a67634cd62e06fd27
Venayak Saxena <venayak.201720@gmail.com>
2026-02-15 00:01:57 +0530

    Chat-Bot

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include<unordered_map>
 #include<string>
 #include<vector>
+#include<cctype>
 
 namespace fs=std::filesystem;
 
@@ -47,16 +48,75 @@ class FileIndexer{
     }
 };
 
+class Chat{
+    public:
+    bool s,o;
+    Chat(bool search, bool open) : s(search), o(open){
+        std::cout<<"Hey! I'm Ven,\nWhat is your name?\n\n\n";
+        std::string name,ans1;
+        std::cin>>name;
+        std::cout<<"\n\nHey "<<name<<"! How can I assist you today?\n\n\n";
+        //while(1)
+        std::cin.ignore();
+        while(ans1.empty()){
+            std::getline(std::cin,ans1);
+            if(ans1.empty()){
+                std::cout<<"\n\nHmm?\n\n\n";
+            }
+            else{
+                std::string word="";
+                for(const char c : ans1){
+                    word+=tolower(c);
+                    if(word=="open"){
+                            o=true;
+                            break;
+                    }
+                    if(word=="search"){
+                            s=true;
+                            break;
+                        }
+                    if(c==' '){
+                        if(word=="open"){
+                            o=true;
+                            break;
+                        }
+                        else if(word=="search"){
+                            s=true;
+                            break;
+                        }
+                        word="";
+                    }
+                }
+            }        
+        }
+    }
+};
+
 int main(){
     std::cout<<"\033[2J\033[1;1H";
+    
+    bool search=false;
+    bool open=false;
+    Chat bot(search,open);
 
-    FileIndexer indexer;
+    if(bot.o){
+        std::cout<<"\n\nreceived open\n\n\n";
+    }
+    else if(bot.s){
+        std::cout<<"\n\nreceived search\n\n\n";
+        
+        FileIndexer indexer;
 
-    std::cout<<"Enter the root directory to index from: (example- C:/users/)\n\n";
-    std::string root;
-    std::getline(std::cin,root);
+        std::cout<<"Enter the root directory to index from: (example- C:/users/)\n\n";
+        std::string root;
+        std::getline(std::cin,root);
 
-    indexer.set_root(root);
-    indexer.build_index();
-    indexer.print_index();
+        indexer.set_root(root);
+        indexer.build_index();
+        indexer.print_index();
+    }
+    
+    std::cin.ignore();
+    std::cout<<"\n\n\n******************************************************************************PRESS ENTER TO EXIT*****************************************************************************\n";
+    std::cin.get();
 }
